78483b9882c3e6802e1802b8083187534ca02ae3
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2019-01-21 15:16:57 +0900

    first commit

---
+++ RxMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxMVVM
+//
+//  Created by 佐藤賢 on 2019/01/21.
+//  Copyright © 2019 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
